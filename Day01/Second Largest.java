32f78cf6fbf3cf3910bf624e8951ade0640429a8
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-11-16 19:42:10 +0530

    Second Largest

---
+++ Day01/Second Largest.java
@@ -0,0 +1,36 @@
+/*Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+Input: arr[] = [10, 10, 10]
+Output: -1
+Explanation: The largest element of the array is 10 and the second largest element does not exist.
+
+Constraints:
+2 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105*/
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int firstLargest = -1;
+        int secondLargest = -1;
+        for(int num : arr){
+            if(num > firstLargest){
+                secondLargest = firstLargest;
+                firstLargest = num;
+            }else if(num < firstLargest && num > secondLargest){
+                secondLargest = num;
+            }
+        }
+        return secondLargest;
+    }
+}
