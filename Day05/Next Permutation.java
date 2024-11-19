cf8ef9b4e3062ef70f60d429879a912e4edb7c35
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-11-19 16:58:48 +0530

    Next Permutation

--- Day05/Next Permutation.java
+++ Day05/Next Permutation.java
@@ -2,7 +2,7 @@
 Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers into the lexicographically next greater permutation. If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
 Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
 
-Examples:
+Examples: 
 
 Input: arr = [2, 4, 1, 7, 5, 0]
 Output: [2, 4, 5, 0, 1, 7]
