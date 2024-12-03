38454915146017ed1b3c14481a854dbb8f2619ca
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-12-03 20:13:16 +0530

    Min Chars to Add for Palindrome

--- Day19/Min Chars to Add for Palindrome.java
+++ Day19/Min Chars to Add for Palindrome.java
@@ -1,6 +1,6 @@
 /*Min Chars to Add for Palindrome
 Given a string s, the task is to find the minimum characters to be added at the front to make the string palindrome.
-Note: A palindrome string is a sequence of characters that reads the same forward and backward.
+Note: A palindrome string is a sequence of characters that reads the same forward and backward. 
 
 Examples:
 
