a8419bf416e92f1765315d2c7a9b70d4c859738f
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-05-18 18:48:16 +0530

    Level Order in spiral form

---
+++ Day185/Level Order in spiral form.java
@@ -0,0 +1,78 @@
+/*Level Order in spiral form
+Given a binary tree and the task is to find the spiral order traversal of the tree and return the list containing the elements.
+Spiral order Traversal mean: Starting from level 0 for root node, for all the even levels we print the node's value from right to left and for all the odd levels we print the node's value from left to right.
+For below tree, function should return [1, 2, 3, 4, 5, 6, 7]
+
+Examples:
+
+Input: root = [1, 3, 2]
+Output: [1, 3, 2]
+Explanation: Start with root (1), print level 0 (right to left), then level 1 (left to right).
+
+Input: root = [10, 20, 30, 40, 60]
+Output: [10, 20, 30, 60, 40]
+Explanation: Start with root (10), print level 0 (right to left), level 1 (left to right), and continue alternating.
+
+Input: root = [1, 2, N, 4]
+Output: [1, 2, 4]
+Explanation: Start with root (1), then level 1 (left to right), then level 2 (right to left).
+
+Constraints:
+
+1 <= number of nodes <= 10^5
+0 <= node->data <= 10^5*/
+
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    public ArrayList<Integer> findSpiral(Node root) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        if(root == null){
+            return res;
+        }
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        
+        boolean rev = false;
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            
+            for(int i=0;i<size;i++){
+                Node curr = queue.poll();
+                level.add(curr.data);
+                
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
+            }
+            
+            if(!rev){
+                Collections.reverse(level);
+            }
+            
+            res.addAll(level);
+            rev = !rev;
+        }
+        
+        return res;
+    }
+}
