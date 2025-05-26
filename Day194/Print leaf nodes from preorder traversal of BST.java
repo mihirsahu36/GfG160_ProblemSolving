200b2b84a017eabe9e64bbabf55ee373afe0558c
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-05-27 00:59:31 +0530

    Print leaf nodes from preorder traversal of BST

---
+++ Day194/Print leaf nodes from preorder traversal of BST.java
@@ -0,0 +1,57 @@
+/*Print leaf nodes from preorder traversal of BST
+Given a preorder traversal of a BST, find the leaf nodes of the tree without building the tree.
+
+Examples:
+
+Input: preorder[] = [5, 2, 10]
+Output: [2, 10]
+Explaination: 
+2 and 10 are the leaf nodes as shown in the figure.
+
+Input: preorder[] = [4, 2, 1, 3, 6, 5]
+Output: [1, 3, 5]
+Explaination: 
+1, 3 and 5 are the leaf nodes as shown in the figure.
+
+Input: preorder[] = [8, 2, 5, 10, 12]
+Output: [5, 12]
+Explaination: 
+5 and 12 are the leaf nodes as shown in the figure.
+
+Constraints:
+
+1 ≤ preorder.size() ≤ 10^3
+1 ≤ preorder[i] ≤ 10^3*/
+
+class Solution {
+    public ArrayList<Integer> leafNodes(int[] preorder) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        int n = preorder.length;
+        
+        for(int i=0;i<n-1;i++){
+            boolean isLeaf = false;
+            
+            if(preorder[i+1] < preorder[i]){
+                stack.push(preorder[i]);
+            }else{
+                while(!stack.isEmpty()){
+                    if(preorder[i+1] > stack.peek()){
+                        stack.pop();
+                        isLeaf = true;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            
+            if(isLeaf){
+                res.add(preorder[i]);
+            }
+        }
+        
+        res.add(preorder[n-1]);
+        return res;
+    }
+}
