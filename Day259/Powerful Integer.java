f035491e25ae9d339aec3dd2563573d08e224e1d
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-07-31 22:01:33 +0530

    Powerful Integer

---
+++ Day259/Powerful Integer.java
@@ -0,0 +1,58 @@
+/* Powerful Integer
+You are given a 2D integer array intervals[][] of length n, where each intervals[i] = [start, end] represents a closed interval (i.e., all integers from start to end, inclusive).
+You are also given an integer k. An integer is called Powerful if it appears in at least k intervals. Find the maximum Powerful Integer.
+Note: If no integer occurs at least k times return -1.
+
+Examples:
+
+Input : n = 3, intervals[][] = [[1, 3], [4, 6], [3, 4]], k = 2
+Output: 4
+Explanation: Integers 3 and 4 appear in 2 intervals. The maximum is 4.
+
+Input : n = 4, intervals[][] = [[1, 4], [12, 45], [3, 8], [10, 12]], k = 3
+Output: -1
+Explanation: No integer appears in at least 3 intervals.
+
+Input : n = 5, intervals[][] = [[16, 21], [5, 8], [12, 17], [17, 29], [9, 24]], k = 3
+Output: 21
+Explanation: Integers 16, 17, 18, 19, 20 and 21 appear in at least 3 intervals. The maximum is 21.
+
+Constraints:
+
+1 ≤ n ≤ 10^5
+1 ≤ intervals[i][0] ≤ intervals[i][1] ≤ 10^9
+1 ≤ k ≤ 10^5 */
+
+class Solution {
+    public int powerfulInteger(int[][] intervals, int k) {
+        // code here
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        
+        for(int []interval : intervals){
+            map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+            map.put(interval[1] + 1, map.getOrDefault(interval[1] + 1, 0) - 1);
+        }
+        
+        int res = -1;
+        int temp = 0;
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int point = entry.getKey();
+            int delta = entry.getValue();
+            
+            if(delta >= 0){
+                temp += delta;
+                if(temp >= k){
+                    res = point;
+                }
+            }else{
+                if(temp >= k){
+                    res = point - 1;
+                }
+                temp += delta;
+            }
+        }
+        
+        return res;
+    }
+}
