0c48a269e3f5c40bc83e63e19ffe8a1ffb6c0df6
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-08-11 21:44:39 +0530

    Palindrome SubStrings

---
+++ Day269/Palindrome SubStrings.java
@@ -0,0 +1,53 @@
+/* Palindrome SubStrings
+Given a string s, count all palindromic sub-strings present in the string.
+The length of the palindromic sub-string must be greater than or equal to 2.
+Note: A substring is palindromic if it reads the same forwards and backwards.
+
+Examples:
+
+Input: s = "abaab"
+Output: 3
+Explanation: All palindromic substrings (of length > 1) are: "aba", "aa", "baab".
+
+Input: s = "aaa"
+Output: 3
+Explanation: All palindromic substrings (of length > 1) are: "aa", "aa", "aaa".
+
+Input: s = "abbaeae"
+Output: 4
+Explanation: All palindromic substrings (of length > 1) are: "bb", "abba", "aea", "eae".
+
+Constraints:
+
+2 ≤ s.size() ≤ 5 * 10^3
+s contains only lowercase english characters */
+
+class Solution {
+    public int countPS(String s) {
+        // code here
+        int n = s.length();
+        int count = 0;
+        
+        for(int i=0;i<n;i++){
+            int left = i - 1;
+            int right = i + 1;
+            while( left >= 0 && right < n && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            int left = i;
+            int right = i + 1;
+            while( left >= 0 && right < n && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
+            }
+        }
+        
+        return count;
+    }
+}
