0b45a0a23d3aa60c03f46037d4554aa0a379fbd9
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-08-14 20:46:12 +0530

    Count Reverse Pairs

---
+++ Day273/Count Reverse Pairs.java
@@ -0,0 +1,78 @@
+/* Count Reverse Pairs
+You are given an array arr[] of positive integers, find the count of reverse pairs.
+A pair of indices (i, j) is said to be a reverse pair if both the following conditions are met:
+0 ≤ i < j < arr.size()
+arr[i] > 2 * arr[j]
+
+Examples:
+
+Input: arr[] = [3, 2, 4, 5, 1, 20]
+Output: 3
+Explanation:
+The Reverse pairs are 
+(0, 4), arr[0] = 3, arr[4] = 1, 3 > 2*1 
+(2, 4), arr[2] = 4, arr[4] = 1, 4 > 2*1 
+(3, 4), arr[3] = 5, arr[4] = 1, 5 > 2*1 
+
+Input: arr[] = [5, 4, 3, 2, 2]
+Output: 2
+Explanation:
+The Reverse pairs are
+(0, 3), arr[0] = 5, arr[3] = 2, 5 > 2*2
+(0, 4), arr[0] = 5, arr[4] = 2, 5 > 2*2
+
+Constraints:
+
+1 ≤ arr.size() ≤ 5*10^4
+1 ≤ arr[i] ≤ 10^9 */
+
+class Solution {
+    public int countRevPairs(int[] arr) {
+        // code here
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+    
+    private int mergeSort(int []arr, int left, int right){
+        if(left >= right){
+            return 0;
+        }
+        
+        int mid = left + (right - left) / 2;
+        int count = mergeSort(arr, left, mid) + mergeSort(arr, mid + 1, right);
+        
+        int j = mid + 1;
+        for(int i=left;i<=mid;i++){
+            while(j <= right && (long)arr[i] > 2L * arr[j]){
+                j++;
+            }
+            count += (j - (mid + 1));
+        }
+        
+        merge(arr, left, mid, right);
+        
+        return count;
+    }
+    
+    private void merge(int []arr, int left, int mid, int right){
+        int []temp = new int[right-left+1];
+        int i = left, j = mid + 1, k = 0;
+        
+        while(i <= mid && j <= right){
+            if(arr[i] <= arr[j]){
+                temp[k++] = arr[i++];
+            }else{
+                temp[k++] = arr[j++];
+            }
+        }
+        
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+        
+        while(j <= right){
+            temp[k++] = arr[j++];
+        }
+        
+        System.arraycopy(temp, 0, arr, left, temp.length);
+    }
+}
