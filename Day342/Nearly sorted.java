db13100f3b0177c7f57b5ef851292783e99e59e9
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-10-22 21:16:36 +0530

    Nearly sorted

---
+++ Day342/Nearly sorted.java
@@ -0,0 +1,47 @@
+/* Nearly sorted
+Given an array arr[], where each element is at most k positions away from its correct position in the sorted order.
+Your task is to restore the sorted order of arr[] by rearranging the elements in place.
+Note: Don't use any sort() method.
+
+Examples:
+
+Input: arr[] = [2, 3, 1, 4], k = 2
+Output: [1, 2, 3, 4]
+Explanation: All elements are at most k = 2 positions away from their correct positions.
+Element 1 moves from index 2 to 0
+Element 2 moves from index 0 to 1
+Element 3 moves from index 1 to 2
+Element 4 stays at index 3
+
+Input: arr[]= [7, 9, 14], k = 1
+Output: [7, 9, 14]
+Explanation: All elements are already stored in the sorted order.
+
+Constraints:
+
+1 ≤ arr.size() ≤ 10^6
+0 ≤ k < arr.size()
+1 ≤ arr[i] ≤ 10^6 */
+
+class Solution {
+    public void nearlySorted(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0;i<=k;i++){
+            pq.add(arr[i]);
+        }
+        
+        int idx = 0;
+        
+        for(int i=k+1;i<n;i++){
+            arr[idx++] = pq.poll();
+            pq.add(arr[i]);
+        }
+        
+        while(!pq.isEmpty()){
+            arr[idx++] = pq.poll();
+        }
+    }
+}
