32db1623f54313348a6ce0ab5b2b9bb6180ee992
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-11-26 02:44:47 +0530

    Game of XOR

---
+++ Day376/Game of XOR.java
@@ -0,0 +1,47 @@
+/* Game of XOR
+You are given an integer array arr[]. The value of a subarray is defined as the bitwise XOR of all elements in that subarray.
+Your task is to compute the bitwise XOR of the values of all possible subarrays of arr[].
+
+Examples:
+
+Input: arr[] = [1, 2, 3] 
+Output: 2
+Explanation:
+xor[1] = 1
+xor[1, 2] = 3
+xor[2, 3] = 1
+xor[1, 2, 3] = 0
+xor[2] = 2
+xor[3] = 3
+Result : 1 ^ 3 ^ 1 ^ 0 ^ 2 ^ 3 = 2
+
+Input: arr[] = [1, 2]
+Output: 0
+Explanation:
+xor[1] = 1
+xor[1, 2] = 3
+xor[2] = 2
+Result : 1 ^ 3 ^ 2 = 0
+
+Constraints:
+
+1 ≤ arr.size() ≤ 10^5
+0 ≤ arr[i] ≤ 10^9 */
+
+
+class Solution {
+    public int subarrayXor(int[] arr) {
+        // code here
+        int n = arr.length;
+        int res = 0;
+
+        for(int i=0;i<n;i++){
+            long count = (long)(i + 1) * (n - i);
+            
+            if((count & 1) == 1){
+                res ^= arr[i];
+            }
+        }
+        return res;
+    }
+}
