831cf873787dfd7abba12bf48a693b718b07b11a
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-11-26 02:47:35 +0530

    AND In Range

---
+++ Day377/AND In Range.java
@@ -0,0 +1,32 @@
+/* AND In Range
+You are given two integers l and r. Find the result after applying the series of Bitwise AND ( & ) operation on every natural number between the range l to r (including both).
+
+Examples:
+
+Input: l = 8, r = 13
+Output: 8
+Explanation: 
+8 AND 9 AND 10 AND 11 AND 12 AND 13 = 8.
+
+Input: l = 2, r = 3
+Output: 2
+Explanation: 2 AND 3 = 2.
+
+Constraints:
+
+1 ≤ l ≤ r ≤ 10^9 */
+
+class Solution {
+    public int andInRange(int l, int r) {
+        // Code here
+        int shifts = 0;
+
+        while(l < r){
+            l >>= 1;
+            r >>= 1;
+            shifts++;
+        }
+
+        return l << shifts;
+    }
+}
