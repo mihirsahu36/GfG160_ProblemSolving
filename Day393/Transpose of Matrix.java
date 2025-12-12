bf2695bcf978b6a98ba786ebccab7d150c0b3f1f
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2025-12-12 21:43:23 +0530

    Transpose of Matrix

--- Day393/Transpose of Matrix.java
+++ Day393/Transpose of Matrix.java
@@ -25,3 +25,21 @@ Constraints:
 
 1 ≤ n ≤ 10^3
 -10^9 ≤ mat[i][j] ≤10^9 */
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> transpose(int[][] mat) {
+        // code here
+        int n = mat.length;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            ArrayList<Integer> row = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                row.add(mat[j][i]);
+            }
+            res.add(row);
+        }
+        
+        return res;
+    }
+  }
